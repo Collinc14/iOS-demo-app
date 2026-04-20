e700b21b993260ea8ce507e69d7fd4d4e8f9b404
BT_C09_04 <mobileappdev@bths.edu>
2026-04-20 14:40:03 -0400

    added the SampleView file

---
+++ Task30Chan_GitWithXcode/SampleView.swift
@@ -0,0 +1,18 @@
+//
+//  SampleView.swift
+//  Task30Chan_GitWithXcode
+//
+//  Created by Student on 4/20/26.
+//
+
+import SwiftUI
+
+struct SampleView: View {
+    var body: some View {
+        Text("Look at this sample view!")
+    }
+}
+
+#Preview {
+    SampleView()
+}
