a62f6adb231d61b9deacfaf2a959f53f9ad01952
BT_C09_04 <mobileappdev@bths.edu>
2026-04-20 14:25:36 -0400

    Initial Commit

---
+++ Task30Chan_GitWithXcode/Task30Chan_GitWithXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task30Chan_GitWithXcodeApp.swift
+//  Task30Chan_GitWithXcode
+//
+//  Created by Student on 4/20/26.
+//
+
+import SwiftUI
+
+@main
+struct Task30Chan_GitWithXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
